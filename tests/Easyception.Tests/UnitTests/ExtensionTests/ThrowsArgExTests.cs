141cca235bafa0e1fc1bd65a711a850e98eae9d6
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-02 23:19:48 -0500

    Added ArgEx and Tests; Moved Tests; Removed Null Checks

---
+++ tests/Easyception.Tests/UnitTests/ExtensionTests/ThrowsArgExTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easyception.Tests
+{
+	[TestFixture]
+	public static class ThrowsArgExTests
+	{
+		//[Test]
+		//public stat
+	}
+}
